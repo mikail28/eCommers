16d6c3d6c99a54cb844aab298b41819ae852a1c6
huzey <huzey@MIKAIL>
2023-03-13 23:48:48 +0400

    Add project files.

---
+++ eCommers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eCommers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
